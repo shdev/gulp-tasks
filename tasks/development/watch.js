23ec1e632a1fec27aa42019421ed9ae3759dd8f6
Sebastian Holtz <mail@sh-dev.de>
2015-01-17 23:24:26 +0100

    added manifest to watch

--- tasks/development/watch.js
+++ tasks/development/watch.js
@@ -5,8 +5,8 @@ var config = require('../../../gulpconfig').watch;
  * Start browsersync task and then watch files for changes
  */
 gulp.task('watch', ['build'], function() {
-	gulp.watch(config.sass, ['sass']);
-	gulp.watch(config.files, ['copy:file']);
-	gulp.watch(config.bower, ['copy:bower']);
-	gulp.watch(config.coffee, ['coffee']);
+	gulp.watch(config.sass, ['sass', 'manifest']);
+	gulp.watch(config.files, ['copy:file', 'manifest']);
+	gulp.watch(config.bower, ['copy:bower','manifest']);
+	gulp.watch(config.coffee, ['coffee', 'manifest']);
 });
